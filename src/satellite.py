5e46eea77834c7ea207c88e9e9af14452f36b01a
g-thn <gillestahan@gmail.com>
2026-02-13 17:11:09 +0100

    Addes equation and plot feature, incomplete not working

--- src/satellite.py
+++ src/satellite.py
@@ -1,3 +1,4 @@
+from math import dist
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -64,6 +65,9 @@ class Satellite:
         self.kpthetadot = 1.0
         self.kdthetadot = 1.0
         self.kithetadot = 1.0
+
+        # Plot parameters and variables
+        self.generateSatGeometry()
 
     def __str__(self):
         """
@@ -166,6 +170,30 @@ class Satellite:
         """
         self.thrustSeq = thrustSeq
 
+    def rotMatrix(self,y):
+        """
+        Returns the rotation matrix for the angle of the state vector
+        args:
+            None 
+        returns:
+            R: rotation matrix
+        """
+        self.R = np.array([[np.cos(y[2]), -np.sin(y[2])],
+                      [np.sin(y[2]), np.cos(y[2])]])
+        return self.R
+    
+    def udpateRotMatrix(self):
+        """
+        Updates the rotation matrix for the angle of the state vector
+        args:
+            None
+        returns:
+            None
+        """        
+        self.R = np.array([[np.cos(self.stVec[-1,2]), -np.sin(self.stVec[-1,2])],
+                           [np.sin(self.stVec[-1,2]),  np.cos(self.stVec[-1,2])]])
+        return self.R
+
     def control(self, t, y):
         """
         Sets the control inputs for the thruster of the satellite in case of a PID controller
@@ -178,8 +206,7 @@ class Satellite:
             self.nNext += 1
             self.tfCurrent = self.tfAll[self.nCurrent]
             self.tfNext = self.tfAll[self.nNext]
-        self.cmdTmp = self.cmdAll[(self.cmdAll[:,0] > self.tfCurrent), :][0,:]
-        
+        self.cmd = self.cmdAll[(self.cmdAll[:,0] > self.tfCurrent), :][0,:]
 
     def eqGenerator(self):
         """
@@ -208,9 +235,9 @@ class Satellite:
             ydot[0] = y[3]
             ydot[1] = y[4]
             ydot[2] = y[5]
-            ydot[3] = self.cmdTmp[0]*np.sin(y[2]+self.cmdTmp[1])/self.mass
-            ydot[4] = self.cmdTmp[0]*np.cos(y[2]+self.cmdTmp[1])/self.mass - self.g
-            ydot[5] = self.cmdTmp[0]*np.sin(self.cmdTmp[1])*self.com/self.inertia
+            ydot[3] = self.thrust*np.cos(self.stVec[-1,2])*(self.cmdTmp[0]+self.cmdTmp[1]-(self.cmdTmp[2]+self.cmdTmp[3]))/self.mass
+            ydot[4] = self.thrust*np.sin(self.stVec[-1,2])*(self.cmdTmp[0]+self.cmdTmp[1]-(self.cmdTmp[2]+self.cmdTmp[3]))/self.mass
+            ydot[5] = self.thrust*self.dist*(self.cmdTmp[0]+self.cmdTmp[2]-(self.cmdTmp[1]+self.cmdTmp[3]))/self.mass
             return ydot
         self.eq = eq
     
@@ -224,6 +251,7 @@ class Satellite:
         self.stVec = np.vstack((self.stVec, y))
         self.cmd = np.vstack((self.cmd, self.cmdTmp))
         self.t = np.append(self.t, t)
+        self.udpateRotMatrix()
     
     def plot(self):
         """
@@ -282,28 +310,88 @@ class Satellite:
 
     def plotControl(self):
         """
-        Plots the control inputs
+        Plots the control  Fa, Fb, Fc and Fd on the same graph
         args:
             None
         """
-        fig, axs = plt.subplots(2, 1)
-        # Make the figure large
-        fig.set_size_inches(18.5, 10.5)
+        fig, ax = plt.subplots()
+        ax.plot(self.t, self.cmd[:, 0], label='Fa')
+        ax.plot(self.t, self.cmd[:, 1], label='Fb')
+        ax.plot(self.t, self.cmd[:, 2], label='Fc')
+        ax.plot(self.t, self.cmd[:, 3], label='Fd')
+        ax.set_title('Thruster Commands')
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Thrust (N)')
+        ax.legend() 
 
-        # Separate the plots
-        fig.tight_layout(pad=3.0)
-
-        axs[0].plot(self.t, self.cmd[:, 0])
-        axs[0].set_title('F (N)')
-        axs[0].set_xlabel('Time (s)')
-        axs[0].set_ylabel('Force (N)')
-
-        axs[1].plot(self.t, self.cmd[:, 1]*180/np.pi)
-        axs[1].set_title('delta (deg)')  
-        axs[1].set_xlabel('Time (s)')
-        axs[1].set_ylabel('Angle (deg)')
 
         plt.show()
+    def generateSatGeometry(self):
+        """
+        Generates the geometry of the satellite for plotting
+        args:
+            None
+        returns:
+            None
+        """
+        self.satSquare = np.array([[self.dist, self.dist],
+                     [self.dist, -self.dist],
+                     [-self.dist, -self.dist],
+                     [-self.dist, self.dist],
+                     [self.dist, self.dist]])
+        self.tGeom = .2*np.array([[0, 0],
+                    [ .5*self.dist, self.dist],
+                    [-.5*self.dist, self.dist],
+                    [       0,    0]])
+
+        self.plume = .2*np.array([[.5*self.dist, self.dist],
+                            [0, 3*self.dist],
+                            [-.5*self.dist, self.dist],
+                            [.5*self.dist, self.dist]])
+
+        self.ta = -self.tGeom + np.array([[self.dist, -self.dist],
+                                [self.dist, -self.dist],
+                                [self.dist, -self.dist],
+                                [self.dist, -self.dist]])
+        self.tb = -self.tGeom + np.array([[-self.dist, -self.dist],
+                                [-self.dist, -self.dist],
+                                [-self.dist, -self.dist],
+                                [-self.dist, -self.dist]])
+        self.tc =  self.tGeom + np.array([[-self.dist, self.dist],
+                                [-self.dist, self.dist],
+                                [-self.dist, self.dist],
+                                [-self.dist, self.dist]])
+        self.td =  self.tGeom + np.array([[self.dist, self.dist],
+                                [self.dist, self.dist],
+                                [self.dist, self.dist],
+                                [self.dist, self.dist]])
+        self.pa =-self.plume + np.array([[self.dist, -self.dist],
+                            [self.dist, -self.dist],
+                            [self.dist, -self.dist],
+                            [self.dist, -self.dist]])
+        self.pb =-self.plume + np.array([[-self.dist, -self.dist],
+                            [-self.dist, -self.dist],  
+                            [-self.dist, -self.dist],
+                            [-self.dist, -self.dist]])
+        self.pc = self.plume + np.array([[-self.dist, self.dist],
+                            [-self.dist, self.dist],
+                            [-self.dist, self.dist],
+                            [-self.dist, self.dist]])
+        self.pd = self.plume + np.array([[self.dist, self.dist],
+                            [self.dist, self.dist],
+                            [self.dist, self.dist],
+                            [self.dist, self.dist]])
+    
+    def rotateSatDrawing(self, drawing ,theta):
+        """
+        Rotates the geometry of the satellite for plotting
+        args:
+            theta: angle of rotation in radians
+        returns:
+            None
+        """        
+        R = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
+        return np.dot(np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]]), drawing.T).T
 
     def plot2D(self):
         """
@@ -314,18 +402,10 @@ class Satellite:
         fig = plt.figure()
         ax = plt.axes(xlim=(-0.1, 5), ylim=(-0.5, 0.5))
         ax.set_aspect('equal')
-        x_top = self.com*np.sin(self.stVec[:, 2])
-        y_top = self.com*np.cos(self.stVec[:, 2])
-        x_bot = -self.com*np.sin(self.stVec[:, 2])
-        y_bot = -self.com*np.cos(self.stVec[:, 2])
-        x_force = self.cmd[:,0]*np.sin(self.stVec[:, 2]+self.cmd[:,1])*0.05
-        y_force = self.cmd[:,0]*np.cos(self.stVec[:, 2]+self.cmd[:,1])*0.05
+        ax.grid(True)
         
-        for i in range(len(self.t),10):
-            plt.plot([self.stVec[i, 0]+x_top[i], self.stVec[i, 0]+x_bot[i]], 
-                     [self.stVec[i, 1]+y_top[i], self.stVec[i, 1]+y_bot[i]], color='red')
-            plt.plot([self.stVec[i, 0]+x_top[i], self.stVec[i, 0]+x_top[i]+x_force[i]], 
-                     [self.stVec[i, 1]+y_top[i], self.stVec[i, 1]+y_top[i]+y_force[i]], color='blue')
+            
+
         plt.show()
 
     def animate(self):
